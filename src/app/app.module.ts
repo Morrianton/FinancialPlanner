3d2993a517afe67f918ccdd2ceb24512872ef411
Morrianton1985 <morrianton@gmail.com>
2018-11-21 22:14:03 -0700

    imported & added FinancialCalculatorsComponent & InterestCalculatorComponent to @NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FinancialCalculatorsComponent } from './financial-calculators/financial-calculators.component';
+import { InterestCalculatorComponent } from './interest-calculator/interest-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinancialCalculatorsComponent,
+    InterestCalculatorComponent
   ],
   imports: [
     BrowserModule
