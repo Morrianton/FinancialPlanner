c7648cf72424a71c7c9892c20ed5832484b6dada
Morrianton1985 <morrianton@gmail.com>
2018-11-21 23:32:55 -0700

    connected form values & created function to calculate interest

--- src/app/interest-calculator/interest-calculator.component.ts
+++ src/app/interest-calculator/interest-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'interest-calculator',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interest-calculator.component.css']
 })
 export class InterestCalculatorComponent implements OnInit {
+principle = this.principle;
+interest = this.interest;
+timesCompounded = this.timesCompounded;
+term = this.term;
+result = this.result;
 
   constructor() { }
+
+  onSubmit(interestForm: NgForm) {
+    this.principle = Number(interestForm.value.principle);
+    this.interest = Number(interestForm.value.interest);
+    this.timesCompounded = Number(interestForm.value.timesCompounded);
+    this.term = Number(interestForm.value.term);
+
+    this.result = this.principle * Math.pow((1 + (this.interest/this.timesCompounded)),(this.timesCompounded * this.term))
+  }
 
   ngOnInit() {
   }
